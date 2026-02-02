48b34b942b2b44abb5a703c2a69f4a73d0d35649
Ildar Kalimullin <salavatvaliev@mail.ru>
2026-02-02 22:04:55 +0300

    second commit

--- src/test/java/StatusTests.java
+++ src/test/java/StatusTests.java
@@ -1,19 +1,29 @@
 import io.restassured.matcher.ResponseAwareMatcher;
 import io.restassured.response.Response;
 import org.hamcrest.Matcher;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
-import static net.bytebuddy.matcher.ElementMatchers.is;
+
 
 
 public class StatusTests {
 
+
+
     @Test
     void checkTotal20() {
-
         get("https://selenoid.autotests.cloud/status")
                 .then()
-                .body("total", is(20));
+                .body("total", Is.is(5))
+                .log().all();
+
+
+
+
+
+
+
 
 
 
